b413516a69a649244e13a83e93c0c264fa62fd8c
Rcv-ThanhNhan <nguyen.thanh.nhan.rcvn2012@gmail.com>
2022-03-30 13:34:57 +0700

    update

--- laravel_be/resources/views/layouts/head.blade.php
+++ laravel_be/resources/views/layouts/head.blade.php
@@ -9,8 +9,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title')</title>
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <!-- vendor css -->
+    <link href="{{ asset('lib/ionicons/css/ionicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/typicons.font/typicons.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- azia CSS -->
     <link rel="stylesheet" href="{{ asset('css/azia.css') }}">
@@ -18,9 +20,6 @@
 
 
     {{-- #### API ##### --}}
-    <script>
-        window.APP_API = `{{ env('APP_API') }}`;
-    </script>
     <script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
 
     @yield('api')
